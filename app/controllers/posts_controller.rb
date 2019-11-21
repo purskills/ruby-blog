7c8731ab23e48df41ea5444a244040ab8ba70293
AngularMast <robertodallasta.dev@gmail.com>
2019-11-21 00:32:57 -0300

    edit and destory

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,15 +8,40 @@ class PostsController < ApplicationController
     end
 
     def new
-    
+        @post = Post.new
     end
 
     def create
         # render plain: params[:post].inspect
         @post = Post.new(post_params)
 
-        @post.save
-        redirect_to @post
+        if(@post.save)
+            redirect_to @post
+        else
+            render 'new'
+        end
+    end
+
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        @post = Post.find(params[:id])
+
+        if(@post.update(post_params))
+            redirect_to @post
+        else
+            render 'edit'
+        end
+    end
+
+    def destory
+        @post = Post.find(params[:id])
+
+        @post.destory
+
+        redirect_to posts_path
     end
 
     private def post_params
