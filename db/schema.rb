92b90129f2b68343c2fbad496ebed1f9ac6f3878
AngularMast <robertodallasta.dev@gmail.com>
2019-11-22 00:32:03 -0300

    authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_21_033606) do
+ActiveRecord::Schema.define(version: 2019_11_21_172029) do
 
   create_table "comments", force: :cascade do |t|
     t.string "username"
@@ -26,6 +26,17 @@ ActiveRecord::Schema.define(version: 2019_11_21_033606) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "email", null: false
+    t.string "encrypted_password", limit: 128, null: false
+    t.string "confirmation_token", limit: 128
+    t.string "remember_token", limit: 128, null: false
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
   add_foreign_key "comments", "posts"
